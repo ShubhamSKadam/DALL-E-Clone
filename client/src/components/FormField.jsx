48704a02f4ca9ffaf44706faa22c6b50b54342e3
Shubham Kadam <shubhamskadam99@outlook.com>
2023-04-04 16:19:13 +0530

    Finishing Generating Images

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -30,15 +30,15 @@ const FormField = ({
         )}
       </div>
 
-      <input 
-      type={type}
-      id={name}
-      name={name}
-      placeholder={placeholder} 
-      value={value}
-      onChange={handleChange}
-      required
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#4649ff] outline-none block w-full p-3"
       />
     </div>
   );
