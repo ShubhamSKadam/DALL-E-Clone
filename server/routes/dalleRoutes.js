44ece5e1e6f8ae43f188d76b96ab7c9ed42a9ce0
Shubham Kadam <shubhamskadam99@outlook.com>
2023-04-04 17:54:41 +0530

    Billing Bug from OPENAI API

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -32,7 +32,7 @@ router.route("/").post(async (req, res) => {
     res.status(200).json({ photo: image });
   } catch (error) {
     console.log(error);
-    res.status(500).send(error?.response.data.error.message)
+    res.status(500).send(error?.response.data.error.message);
   }
 });
 
